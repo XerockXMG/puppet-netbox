f5c48343cde096775ddfc3afbddcd0d89ee02a83
Daemon Gibson <daemon@cnwr.com>
2018-05-08 10:51:39 -0400

    Maybe

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,7 @@ class netbox::install {
 
   if $netbox::manage_python == true {
 
-    package { lookup('netbox::python::packages'):
+    package { $netbox::python::packages:
       ensure => 'installed',
     }
 
