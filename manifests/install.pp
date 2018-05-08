c64cc4c8426fdc1656a651b44c9445053e425ab2
Daemon Gibson <daemon@cnwr.com>
2018-05-08 12:24:54 -0400

    Virtual

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,7 +13,7 @@ class netbox::install {
       version    => $netbox::python_version,
       pip        => 'present',
       dev        => 'present',
-      virtualenv => 'present',
+      virtualenv => 'absent',
       gunicorn   => 'present',
     }
   }
