7e34ca4a6c785b5279b85447772d43941777afdd
Daemon Gibson <daemon@cnwr.com>
2018-05-14 10:30:28 -0400

    Changes

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,4 +9,29 @@ class netbox::config {
     mode    => '0644',
     require => Vcsrepo["${netbox::directory}"]
   }
+
+  file { 'install script':
+    ensure  => present,
+    content => epp("${module_name}/installation.sh.epp"),
+    path    => "${netbox::directory}/installation.sh",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Vcsrepo["${netbox::directory}"]
+  }
+
+  file { 'expect script':
+    ensure  => present,
+    content => epp("${module_name}/expect.sh.epp"),
+    path    => "${netbox::directory}/expect.sh",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Vcsrepo["${netbox::directory}"]
+  }
+
+  exec { 'installation':
+    command => "/usr/bin/expect ${netbox::directory}/expect.sh",
+    require => [ File['install script'], File['expect script'] ]
+  }
 }
